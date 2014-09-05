a52c1cd6f9f1a8c90a73386c66d1ce7c8aef47d0
Amanda Wagener <amanda@awagener.com>
2014-09-05 21:42:44 +1200

    Separate plugin generation out into its own class.

---
+++ core/lib/refinery/seeds/plugin_generator.rb
@@ -0,0 +1,24 @@
+module Refinery
+  module Seeds
+    class PluginGenerator
+      def initalize(name, options = {})
+        @name = name
+        @users = options[:users] || Refinery::User.all if defined?(Refinery::User)
+      end
+      
+      def generate
+        @users.each do |user|
+          user.plugins.where(name: @name).first_or_create!(
+            position: plugin_position_for_user(user)
+          )
+        end
+      end
+      
+      private
+      
+      def plugin_position_for_user(user)
+        (user.plugins.maximum(:position) || -1) +1
+      end
+    end
+  end
+end
